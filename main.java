3b06eba75d60458e721ae9c0c4b6d94e57e3d30d
ScOwL9 <122111994+ScOwL9@users.noreply.github.com>
2026-04-09 15:18:27 +0200

    Update main.java

--- main.java
+++ main.java
@@ -3,4 +3,15 @@ deux fois. Le joueur avec le plus de points gagne"
 
 faire un compteur de gemmes pour indiquer l'avancée de la partie
 faire un compteur de "passes" : si joueur blanc passe, ajoute 1 au compteur , si joueur noir joue compteur revient a 0, sinon si joueur noir passe ; compteur = 2 fin partie
+
+ "Au debut de la partie, 10 gemmes sont placées aléatoirement, face cachée (une par case
+maximum)"
+
+
+fonction récursive : si gemme dejà placée , placer autre part , sinon placer gemme case sélectionnée*
+
+liste cases , si gemme placée a cette case on supprime la case de la liste
+
+faire liste des gemmes placées pour le png ;
+
 */
