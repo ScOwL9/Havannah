974b5a604a45ba9cdbade78631efd62f8c37852b
MalyarHereThere <igor2016.vladimirov@hotmail.com>
2026-04-09 15:16:27 +0200

    Update INFO Projet.java

--- INFO Projet.java
+++ INFO Projet.java
@@ -5,16 +5,20 @@ public class ProjetInfo {
     // Une liste 5x5
 
     int[][] plat = new int[5][5];
+
+    for(int i = 0; i < plat.length; i++) {
+      for(int j = 0) {}
+    }
     
   }
   
 }
 
-///     ***
-///    *****
-///   *******
-///    *****
-///     ***
+///     **
+///    ****
+///   ******
+///    ****
+///     **
 /// 
 ///         * 
 ///  **    **     
