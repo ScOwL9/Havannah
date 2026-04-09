3b9d486646c3047d02b3e917d451939d21804eca
MalyarHereThere <igor2016.vladimirov@hotmail.com>
2026-04-09 15:01:03 +0200

    Add files via upload

---
+++ Projet Info.java
@@ -0,0 +1,21 @@
+// we have a structure that is hexagonal, and has a size 5
+// a player can build the following structures: triangle, line, star
+
+///     ***
+///    *****
+///   *******
+///    *****
+///     ***
+/// 
+///         * 
+///  **    **     
+///   *      
+/// 
+/// *            *
+///  *           *
+///   *          *
+///    *         *
+///              *
+///     *
+///   *****
+///     *
